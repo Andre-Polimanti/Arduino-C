afa63ce1fa1573d93d5251436963ec3eec08ed66
André Polimanti Mesquiari Lemos <128382081+Andre-Polimanti@users.noreply.github.com>
2025-09-22 22:40:31 -0300

    Add files via upload

---
+++ led-manipulation/3_light_dimmer/3_light_dimmer.ino
@@ -0,0 +1,24 @@
+// This is an improvement of the previous code, with the same eletronic build
+
+// Pin initilization
+const int POTENCIOMETER_PIN = A0;
+const int LED_PIN = 11; // One of the pins that are compatible with pwm
+
+// Global variables
+int reading = 0;
+int pwm = 0;
+
+void setup() {
+  // Pin configuration
+  pinMode(POTENCIOMETER_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  reading = analogRead(POTENCIOMETER_PIN);
+  
+  // converts the data of 8 bits to 6
+  pwm = map(reading, 0, 1023, 0, 255); 
+
+  analogWrite(LED_PIN, pwm);
+}
